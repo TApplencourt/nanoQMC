db728daf3a84cf4760ac1d2ece10dfc588298960
Kevin Gasperich <kgasperich@gmail.com>
2020-07-30 12:07:15 -0500

    indentation

--- spline.cpp
+++ spline.cpp
@@ -50,32 +50,32 @@ inline void getSplineBound(T x, TRESIDUAL& dx, int& ind, int nmax)
 template<typename T>
 inline static void compute_prefactors(T a[4], T da[4], T d2a[4], T tx)
 {
-    //48 total
-    static constexpr T A00 = -1.0/6.0, A01 =  3.0/6.0, A02 = -3.0/6.0, A03 = 1.0/6.0;
-    static constexpr T A10 =  3.0/6.0, A11 = -6.0/6.0, A12 =  0.0/6.0, A13 = 4.0/6.0;
-    static constexpr T A20 = -3.0/6.0, A21 =  3.0/6.0, A22 =  3.0/6.0, A23 = 1.0/6.0;
-    static constexpr T A30 =  1.0/6.0, A31 =  0.0/6.0, A32 =  0.0/6.0, A33 = 0.0/6.0;
-    static constexpr T dA01 = -0.5, dA02 =  1.0, dA03 = -0.5;
-    static constexpr T dA11 =  1.5, dA12 = -2.0, dA13 =  0.0;
-    static constexpr T dA21 = -1.5, dA22 =  1.0, dA23 =  0.5;
-    static constexpr T dA31 =  0.5, dA32 =  0.0, dA33 =  0.0;
-    static constexpr T d2A02 = -1.0, d2A03 =  1.0;
-    static constexpr T d2A12 =  3.0, d2A13 = -2.0;
-    static constexpr T d2A22 = -3.0, d2A23 =  1.0;
-    static constexpr T d2A32 =  1.0, d2A33 =  0.0;
+  //48 total
+  static constexpr T A00 = -1.0/6.0, A01 =  3.0/6.0, A02 = -3.0/6.0, A03 = 1.0/6.0;
+  static constexpr T A10 =  3.0/6.0, A11 = -6.0/6.0, A12 =  0.0/6.0, A13 = 4.0/6.0;
+  static constexpr T A20 = -3.0/6.0, A21 =  3.0/6.0, A22 =  3.0/6.0, A23 = 1.0/6.0;
+  static constexpr T A30 =  1.0/6.0, A31 =  0.0/6.0, A32 =  0.0/6.0, A33 = 0.0/6.0;
+  static constexpr T dA01 = -0.5, dA02 =  1.0, dA03 = -0.5;
+  static constexpr T dA11 =  1.5, dA12 = -2.0, dA13 =  0.0;
+  static constexpr T dA21 = -1.5, dA22 =  1.0, dA23 =  0.5;
+  static constexpr T dA31 =  0.5, dA32 =  0.0, dA33 =  0.0;
+  static constexpr T d2A02 = -1.0, d2A03 =  1.0;
+  static constexpr T d2A12 =  3.0, d2A13 = -2.0;
+  static constexpr T d2A22 = -3.0, d2A23 =  1.0;
+  static constexpr T d2A32 =  1.0, d2A33 =  0.0;
 
-    a[0]   = ((A00 * tx + A01) * tx + A02) * tx + A03;
-    a[1]   = ((A10 * tx + A11) * tx + A12) * tx + A13;
-    a[2]   = ((A20 * tx + A21) * tx + A22) * tx + A23;
-    a[3]   = ((A30 * tx + A31) * tx + A32) * tx + A33;
-    da[0]  = (dA01 * tx + dA02) * tx + dA03;
-    da[1]  = (dA11 * tx + dA12) * tx + dA13;
-    da[2]  = (dA21 * tx + dA22) * tx + dA23;
-    da[3]  = (dA31 * tx + dA32) * tx + dA33;
-    d2a[0] = d2A02 * tx + d2A03;
-    d2a[1] = d2A12 * tx + d2A13;
-    d2a[2] = d2A22 * tx + d2A23;
-    d2a[3] = d2A32 * tx + d2A33;
+  a[0]   = ((A00 * tx + A01) * tx + A02) * tx + A03;
+  a[1]   = ((A10 * tx + A11) * tx + A12) * tx + A13;
+  a[2]   = ((A20 * tx + A21) * tx + A22) * tx + A23;
+  a[3]   = ((A30 * tx + A31) * tx + A32) * tx + A33;
+  da[0]  = (dA01 * tx + dA02) * tx + dA03;
+  da[1]  = (dA11 * tx + dA12) * tx + dA13;
+  da[2]  = (dA21 * tx + dA22) * tx + dA23;
+  da[3]  = (dA31 * tx + dA32) * tx + dA33;
+  d2a[0] = d2A02 * tx + d2A03;
+  d2a[1] = d2A12 * tx + d2A13;
+  d2a[2] = d2A22 * tx + d2A23;
+  d2a[3] = d2A32 * tx + d2A33;
 }
 
 template<typename T>
@@ -221,116 +221,116 @@ int main(int argc, char **argv) {
   // |  (_| | _> (/_   _|_ | | |_) |_| |_
   //                           |
 
-    cxxopts::Options options("nanoqmc",
-                             "miniapps of miniqmc");
+  cxxopts::Options options("nanoqmc",
+                           "miniapps of miniqmc");
 
-    options.add_options()
-            ("h,help", "Print help")
-            ("x", "Size of spline x", cxxopts::value<int>()->default_value("37"))
-            ("y", "Size of spline y", cxxopts::value<int>()->default_value("37"))
-            ("z", "Size of spline z", cxxopts::value<int>()->default_value("37"))
-            ("e,", "Number of electron", cxxopts::value<int>()->default_value("20"))
-            ("b,nblock", "Number of block", cxxopts::value<int>()->default_value("1"))
-            ("n,niter", "Number of dummy iterations", cxxopts::value<int>()->default_value("1"))
-            ("v,verbose", "output level", cxxopts::value<int>()->default_value("0"))
-    ;
+  options.add_options()
+          ("h,help", "Print help")
+          ("x", "Size of spline x", cxxopts::value<int>()->default_value("37"))
+          ("y", "Size of spline y", cxxopts::value<int>()->default_value("37"))
+          ("z", "Size of spline z", cxxopts::value<int>()->default_value("37"))
+          ("e,", "Number of electron", cxxopts::value<int>()->default_value("20"))
+          ("b,nblock", "Number of block", cxxopts::value<int>()->default_value("1"))
+          ("n,niter", "Number of dummy iterations", cxxopts::value<int>()->default_value("1"))
+          ("v,verbose", "output level", cxxopts::value<int>()->default_value("0"))
+  ;
 
-    auto result = options.parse(argc, argv);
+  auto result = options.parse(argc, argv);
 
-    if (result.count("help")) {
-      std::cout << options.help({"", "Group"}) << std::endl;
-      exit(0);
+  if (result.count("help")) {
+    std::cout << options.help({"", "Group"}) << std::endl;
+    exit(0);
+  }
+
+  const int verbose = result["v"].as<int>();
+  const int nx = result["x"].as<int>();
+  const int ny = result["y"].as<int>();
+  const int nz = result["z"].as<int>();
+  const int nelectrons =  result["e"].as<int>();
+  const int niter = result["n"].as<int>();
+
+  //Declaration
+  const int nord = nelectrons  / 2;
+  const int n_splines = nord;
+  const int n_coef = nord * (nx+3) * (ny+3) * (nz+3);
+
+  const int nblock = result["nblock"].as<int>();
+  assert ( n_splines%nblock == 0);
+  
+  const int n_splines_block = n_splines / nblock;
+  
+  std::vector<double> l_start{0.,0.,0.};   
+  std::vector<double> l_end{1.,1.,1.};
+  std::vector<int> l_num{nx,ny,nz};
+
+  std::vector<double> l_delta(3);
+  std::vector<double> l_delta_inv(3);
+
+  for (int i=0; i<3; i++){
+    l_delta[i] =  ( l_end[i] - l_start[i] ) / (double)(l_num[i]) ;
+    l_delta_inv[i] = 1./l_delta[i];
+  }
+
+  std::vector<float> coefs(n_coef);
+  std::vector<std::vector<float>> vals(nelectrons, std::vector<float>(n_splines));
+  std::vector<std::vector<float>> grads(nelectrons, std::vector<float>(n_splines*3));
+  std::vector<std::vector<float>> hess(nelectrons, std::vector<float>(n_splines*6));
+
+  std::vector<float> electron_pos_x(nelectrons);
+  std::vector<float> electron_pos_y(nelectrons);
+  std::vector<float> electron_pos_z(nelectrons);
+
+  // Put random values
+  std::uniform_real_distribution<float> distribution(0.,1.);
+  std::minstd_rand generator(0);
+
+
+  std::generate(coefs.begin(), coefs.end(), [&distribution, &generator]() { return distribution(generator); });
+  std::generate(electron_pos_x.begin(), electron_pos_x.end(), [&distribution, &generator]() { return distribution(generator); });
+  std::generate(electron_pos_y.begin(), electron_pos_y.end(), [&distribution, &generator]() { return distribution(generator); });
+  std::generate(electron_pos_z.begin(), electron_pos_z.end(), [&distribution, &generator]() { return distribution(generator); });
+ 
+  auto s = SplineType {  1, 1, 1,
+                         Ugrid{ l_start[0], l_end[0], l_num[0], l_delta[0], l_delta_inv[0] },
+                         Ugrid{ l_start[1], l_end[1], l_num[1], l_delta[1], l_delta_inv[1] },
+                         Ugrid{ l_start[2], l_end[2], l_num[2], l_delta[2], l_delta_inv[2] },
+                      } ;
+
+  auto start = std::chrono::system_clock::now();
+
+  double nflop = 1.*niter*nelectrons*nblock*(252+665*n_splines_block);
+  // Kernel
+  for (int i=0; i< niter; i++) {
+    for (int e=0 ; e < nelectrons ; e++){
+      std::fill(vals[e].begin(), vals[e].end(), float());
+      std::fill(grads[e].begin(), grads[e].end(), float());
+      std::fill(hess[e].begin(), hess[e].end(), float());
     }
-
-    const int verbose = result["v"].as<int>();
-    const int nx = result["x"].as<int>();
-    const int ny = result["y"].as<int>();
-    const int nz = result["z"].as<int>();
-    const int nelectrons =  result["e"].as<int>();
-    const int niter = result["n"].as<int>();
-
-    //Declaration
-    const int nord = nelectrons  / 2;
-    const int n_splines = nord;
-    const int n_coef = nord * (nx+3) * (ny+3) * (nz+3);
-
-    const int nblock = result["nblock"].as<int>();
-    assert ( n_splines%nblock == 0);
-    
-    const int n_splines_block = n_splines / nblock;
-    
-    std::vector<double> l_start{0.,0.,0.};   
-    std::vector<double> l_end{1.,1.,1.};
-    std::vector<int> l_num{nx,ny,nz};
-
-    std::vector<double> l_delta(3);
-    std::vector<double> l_delta_inv(3);
-
-    for (int i=0; i<3; i++){
-        l_delta[i] =  ( l_end[i] - l_start[i] ) / (double)(l_num[i]) ;
-        l_delta_inv[i] = 1./l_delta[i];
-    }
-
-    std::vector<float> coefs(n_coef);
-    std::vector<std::vector<float>> vals(nelectrons, std::vector<float>(n_splines));
-    std::vector<std::vector<float>> grads(nelectrons, std::vector<float>(n_splines*3));
-    std::vector<std::vector<float>> hess(nelectrons, std::vector<float>(n_splines*6));
-
-    std::vector<float> electron_pos_x(nelectrons);
-    std::vector<float> electron_pos_y(nelectrons);
-    std::vector<float> electron_pos_z(nelectrons);
-
-    // Put random values
-    std::uniform_real_distribution<float> distribution(0.,1.);
-    std::minstd_rand generator(0);
-
-
-    std::generate(coefs.begin(), coefs.end(), [&distribution, &generator]() { return distribution(generator); });
-    std::generate(electron_pos_x.begin(), electron_pos_x.end(), [&distribution, &generator]() { return distribution(generator); });
-    std::generate(electron_pos_y.begin(), electron_pos_y.end(), [&distribution, &generator]() { return distribution(generator); });
-    std::generate(electron_pos_z.begin(), electron_pos_z.end(), [&distribution, &generator]() { return distribution(generator); });
-   
-    auto s = SplineType {  1, 1, 1,
-                           Ugrid{ l_start[0], l_end[0], l_num[0], l_delta[0], l_delta_inv[0] },
-                           Ugrid{ l_start[1], l_end[1], l_num[1], l_delta[1], l_delta_inv[1] },
-                           Ugrid{ l_start[2], l_end[2], l_num[2], l_delta[2], l_delta_inv[2] },
-                        } ;
-
-    auto start = std::chrono::system_clock::now();
-
-    double nflop = 1.*niter*nelectrons*nblock*(252+665*n_splines_block);
-    // Kernel
-    for (int i=0; i< niter; i++) {
-      for (int e=0 ; e < nelectrons ; e++){
-        std::fill(vals[e].begin(), vals[e].end(), float());
-        std::fill(grads[e].begin(), grads[e].end(), float());
-        std::fill(hess[e].begin(), hess[e].end(), float());
+    //nelectrons*nblock*(cLAndF + 102 + 665*n_splines_block)
+    //nelectrons*nblock*(252 + 665*n_splines_block)
+    #pragma omp parallel for collapse(2)
+    for (int e=0 ; e < nelectrons ; e++){
+      for (int b=0; b < nblock; b++) {
+        int offset = b*n_splines_block;
+        evaluate_vgh(&s, coefs.data()+offset,
+            electron_pos_x[e], electron_pos_y[e], electron_pos_z[e],
+            vals[e].data()+offset,
+            grads[e].data()+3*offset,
+            hess[e].data()+6*offset,
+            n_splines_block);
       }
-      //nelectrons*nblock*(cLAndF + 102 + 665*n_splines_block)
-      //nelectrons*nblock*(252 + 665*n_splines_block)
-      #pragma omp parallel for collapse(2)
-      for (int e=0 ; e < nelectrons ; e++){
-          for (int b=0; b < nblock; b++) {
-              int offset = b*n_splines_block;
-              evaluate_vgh(&s, coefs.data()+offset,
-                  electron_pos_x[e], electron_pos_y[e], electron_pos_z[e],
-                  vals[e].data()+offset,
-                  grads[e].data()+3*offset,
-                  hess[e].data()+6*offset,
-                  n_splines_block);
-          }
-      }
     }
-    auto end = std::chrono::system_clock::now();
-    double walltime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-    std::cout << "flop count: " << nflop << std::endl;
-    std::cout << "time: " << walltime << " ns" << std::endl;
-    std::cout << "GFLOPS: " << nflop/walltime << std::endl;
-    if (verbose > 1){
-      std::cout << "val:  " << std::accumulate(vals[0].begin(), vals[0].end(), 0.) << std::endl;
-      std::cout << "grad: " << std::accumulate(grads[0].begin(), grads[0].end(), 0.) << std::endl;
-      std::cout << "hess: " << std::accumulate(hess[0].begin(), hess[0].end(), 0.) << std::endl;
-    }
-    return 0;
+  }
+  auto end = std::chrono::system_clock::now();
+  double walltime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  std::cout << "flop count: " << nflop << std::endl;
+  std::cout << "time: " << walltime << " ns" << std::endl;
+  std::cout << "GFLOPS: " << nflop/walltime << std::endl;
+  if (verbose > 1){
+    std::cout << "val:  " << std::accumulate(vals[0].begin(), vals[0].end(), 0.) << std::endl;
+    std::cout << "grad: " << std::accumulate(grads[0].begin(), grads[0].end(), 0.) << std::endl;
+    std::cout << "hess: " << std::accumulate(hess[0].begin(), hess[0].end(), 0.) << std::endl;
+  }
+  return 0;
 }
 
